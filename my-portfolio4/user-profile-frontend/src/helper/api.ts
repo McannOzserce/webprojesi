c401b35c145e8825c2b8929de9d220704209e7ee
Mert Can Özserçe <97916389+McannOzserce@users.noreply.github.com>
2026-01-08 05:12:50 +0300

    Delete my-portfolio4/user-profile-frontend directory

--- my-portfolio4/user-profile-frontend/src/helper/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "http://localhost:3000", // Sakın sonuna /profiles veya / ekleme
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
